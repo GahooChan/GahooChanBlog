0dc5188fd2636e5cc63043656f3c8cad84396ae3
GahooChen <2432168423@qq.com>
2025-09-26 19:40:31 +0800

    feat:增加侧边栏收纳功能

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -15,50 +15,7 @@ export default sidebar({
       text: "编程语言基础与算法学习",
       icon: "code",
       prefix: "programming-languages/",
-      children: [
-        "",
-        {
-          
-          text: "C语言",
-          prefix: "c/",
-          children: [""],
-        },
-        {
-          text: "C++",
-          prefix: "cpp/",
-          children: [""],
-        },
-        {
-          text: "C#",
-          prefix: "csharp/",
-          children: [""],
-        },
-        {
-          text: "Java",
-          prefix: "java/",
-          children: [""],
-        },
-        {
-          text: "Python",
-          prefix: "python/",
-          children: [""],
-        },
-        {
-          text: "通用概念",
-          prefix: "common-concepts/",
-          children: [""],
-        },
-        {
-          text: "数学计算",
-          prefix: "math-calculations/",
-          children: [""],
-        },
-        {
-          text: "编程工具",
-          prefix: "programming-tools/",
-          children: [""],
-        },
-      ],
+      children: "structure"
     },
   ],
 });
